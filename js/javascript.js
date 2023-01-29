f1c7e01f872c8da0aef71dd4f579948c09d639b1
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-01-29 19:56:02 +0200

    Add comments

--- js/javascript.js
+++ js/javascript.js
@@ -1,44 +1,48 @@
+/*
+    Declare the array of game options.
+
+    The options are located in strict order, so element 'i' beats element 'i - 1'
+    and looses to element 'i + 1'. This logic will be used in further algorithms.
+*/
 const playOptions = ['Rock', 'Paper', 'Scissors']
 
+// Declare function getting random computer game choice
 function getComputerChoice() {
     return playOptions[Math.floor(Math.random() * 3)];
 }
 
+// Declare function that plays one single round of the game
 function playRound(playerChoice, computerChoice) {
     
+    // Check if string is empty
     if (!playerChoice) return 'Invalid input';
     
+    // Convert user input to lowercase string with first letter capital
     let pChoice = playerChoice.toLowerCase();
     pChoice = pChoice.replace(pChoice[0], pChoice[0].toUpperCase());
 
-    if (pChoice === computerChoice) return 'Tie game!';
+    // Get indexes of player and computer choices from game options
+    let playerIndex = playOptions.indexOf(pChoice);
+    let computerIndex = playOptions.indexOf(computerChoice);
+    
+    // Check if user input exist in game choices
+    if (playerIndex === -1) return 'Invalid input';
 
-    let result;
+    // Check if player and computer choices are the same
+    if (playerIndex === computerIndex) return 'Tie game!';
+    
+    // Get index distance
+    let indexDistance = playerIndex - computerIndex;
 
-    switch (pChoice) {
-        case 'Rock':
-            result = (computerChoice === 'Scissors');
-            break;
-        
-        case 'Paper':
-            result = (computerChoice === 'Rock');
-            break;
-        
-        case 'Scissors':
-            result = (computerChoice === 'Paper');
-            break;
-        
-        default:
-            return 'Invalid input';
-    }
-
-    if (result) {
+    // Check game options logic and return round result
+    if (indexDistance === 1 || indexDistance === -(playOptions.length - 1)) {
         return `You Win! ${pChoice} beats ${computerChoice}`;
     } else {
         return `You Lose! ${computerChoice} beats ${pChoice}`;
     }
 }
 
+// Declare function that plays the game (5 rounds)
 function game() {
     for (let i = 0; i < 5; i++) {
         console.log(`%cRound ${i + 1}!\n`, `font-size: 20px;`);
