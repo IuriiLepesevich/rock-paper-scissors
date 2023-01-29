9037d2a733312fe8472d365c80f9e9d963c85a1c
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-01-29 18:30:42 +0200

    Create function that plays a round
    Function takes 2 arguments that represent player and computer choice.
    It checks choices based on the rules of the game and returns a string,
    representing result of the round.

--- js/javascript.js
+++ js/javascript.js
@@ -3,3 +3,38 @@ const playOptions = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice() {
     return playOptions[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerChoice, computerChoice) {
+    let pChoice = playerChoice.toLowerCase();
+    pChoice = pChoice.replace(pChoice[0], pChoice[0].toUpperCase());
+
+    console.log(pChoice);
+    console.log(computerChoice);
+
+    if (pChoice === computerChoice) return 'Tie game!';
+
+    let result;
+
+    switch (pChoice) {
+        case 'Rock':
+            result = (computerChoice === 'Scissors');
+            break;
+        
+        case 'Paper':
+            result = (computerChoice === 'Rock');
+            break;
+        
+        case 'Scissors':
+            result = (computerChoice === 'Paper');
+            break;
+        
+        default:
+            return 'Invalid input';
+    }
+
+    if (result) {
+        return `You Win! ${pChoice} beats ${computerChoice}`;
+    } else {
+        return `You Lose! ${computerChoice} beats ${pChoice}`;
+    }
+}
