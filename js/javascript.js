702c575534c4f3f64be5c0b508894efbafadfb1f
IuriiLepesevich <iurii.lepesevich@gmail.com>
2023-02-01 21:20:57 +0200

    Complete rock-paper-scissors UI implementation

--- js/javascript.js
+++ js/javascript.js
@@ -4,53 +4,112 @@
     The options are located in strict order, so element 'i' beats element 'i - 1'
     and looses to element 'i + 1'. This logic will be used in further algorithms.
 */
-const gameOption = ['Rock', 'Paper', 'Scissors']
+const gameOption = ['Rock', 'Paper', 'Scissors'];
+
+const resultElem = document.querySelector('.result');
+const scoreElem = document.querySelector('.score');
+const buttons = document.querySelectorAll('.buttons');
+
+let compScore = 0;
+let playerScore = 0;
 
 // Declare function getting random computer game choice
 function getComputerChoice() {
     return gameOption[Math.floor(Math.random() * 3)];
 }
 
+function playerWins(playerChoice, computerChoice) {
+    playerScore += 1;
+    resultElem.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+}
+
+function computerWins(playerChoice, computerChoice) {
+    compScore += 1
+    resultElem.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+}
+
+function tieGame() {
+    resultElem.textContent = `Tie game!`;
+}
+
+function dropError() {
+    resultElem.textContent = 'ERROR!'
+}
+
+// Function colors borders of buttons, depending on player/computer choice
+function colorButtonsBorder(playerChoice, computerChoice) {
+    if(playerChoice === computerChoice) {
+        document.querySelector(`.buttons[data-option=
+            ${playerChoice}]`).style.borderColor = 'yellow';
+    } else {
+        document.querySelector(`.buttons[data-option=
+            ${playerChoice}]`).style.borderColor = 'green';
+
+        document.querySelector(`.buttons[data-option=
+            ${computerChoice}]`).style.borderColor = 'red';
+    }
+}
+
+// Function resets game it it's starting state
+function clearButtonsBorderBorder() {
+    buttons.forEach(button => button.style.borderColor = 'black');
+}
+
 // Declare function that plays one single round of the game
 function playRound(playerChoice, computerChoice) {
     
     // Check if string is empty
-    if (!playerChoice) return 'Invalid input';
-    
-    // Convert user input to lowercase string with first letter capital
-    let pChoice = playerChoice.toLowerCase();
-    pChoice = pChoice.replace(pChoice[0], pChoice[0].toUpperCase());
+    if (!playerChoice) return dropError();
 
     // Get indexes of player and computer choices from game options
-    let playerIndex = gameOption.indexOf(pChoice);
+    let playerIndex = gameOption.indexOf(playerChoice);
     let computerIndex = gameOption.indexOf(computerChoice);
     
     // Check if user input exist in game choices
-    if (playerIndex === -1) return 'Invalid input';
+    if (playerIndex === -1) return dropError();
 
     // Check if player and computer choices are the same
-    if (playerIndex === computerIndex) return 'Tie game!';
+    if (playerIndex === computerIndex) return tieGame();
     
     // Get index distance
     let indexDistance = playerIndex - computerIndex;
 
     // Check game options logic and return round result
     if (indexDistance === 1 || indexDistance === -(gameOption.length - 1)) {
-        return `You Win! ${pChoice} beats ${computerChoice}`;
+        return playerWins(playerChoice, computerChoice);
     } else {
-        return `You Lose! ${computerChoice} beats ${pChoice}`;
+        return computerWins(playerChoice, computerChoice);
     }
 }
 
-// Declare function that plays the game (5 rounds)
-function game() {
-    console.clear();
-    for (let i = 0; i < 5; i++) {
-        console.log(`%cRound ${i + 1}!\n`, `font-size: 20px;`);
-        console.log(playRound(prompt("Rock, Scissors or Paper?"),
-        getComputerChoice()));
-    }
+// Function resets game it it's starting state
+function resetGame() {
+    resultElem.style.color = 'black';
+    resultElem.textContent = "To start - press a button";
+    compScore = 0;
+    playerScore = 0;
+    scoreElem.textContent = `${playerScore}-${compScore}`;
+    clearButtonsBorder();
 }
 
-const button = document.querySelector("#startGame");
-button.addEventListener('click', game);
+buttons.forEach(button => button.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const playerChoice = button.dataset.option;
+
+    clearButtonsBorder();
+    colorButtonsBorder(playerChoice, computerChoice);
+
+    playRound(playerChoice, computerChoice);
+
+    scoreElem.textContent = `${playerScore}-${compScore}`
+
+    if (playerScore >= 5) {
+        resultElem.style.color = 'green';
+        resultElem.textContent = "You won! Press button to play again.";
+        button.addEventListener('click', resetGame, {once: true});
+    } else if (compScore >= 5) {
+        resultElem.style.color = 'red';
+        resultElem.textContent = "You lost! Press button to play again.";
+        button.addEventListener('click', resetGame, {once: true});
+    }
+}));
